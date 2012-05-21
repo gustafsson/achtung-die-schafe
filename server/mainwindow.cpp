5e0e3b9e070a3a566271aa88e6ecc2fdc2d7fc91
Johan Gustafsson <johan.gustafsson@muchdifferent.com>
2012-05-21 02:21:54 +0200

    In the server window, list players with score active play time

--- server/mainwindow.cpp
+++ server/mainwindow.cpp
@@ -43,14 +43,14 @@ MainWindow::~MainWindow()
 void MainWindow::newPlayer(PlayerId id, QString name)
 {
     world.newPlayer(id,name);
-    ui->listWidget->addItem(QString("Player %1 joined").arg(id));
+    ui->labelNumberOfPlayers->setText(QString("%1").arg(ui->labelNumberOfPlayers->text().toInt()+1));
 }
 
 
 void MainWindow::lostPlayer(PlayerId id)
 {
+    ui->labelNumberOfPlayers->setText(QString("%1").arg(ui->labelNumberOfPlayers->text().toInt()-1));
     world.lostPlayer(id);
-    ui->listWidget->addItem(QString("Player %1 left").arg(id));
 }
 
 
@@ -60,6 +60,12 @@ void MainWindow::gotPlayerData(PlayerId id, QString data)
     p->userData(data, &world);
 }
 
+bool highScore(const pPlayer& a, const pPlayer& b)
+{
+    // Sort descending
+    return a->score > b->score;
+}
+
 
 void MainWindow::timestep()
 {
@@ -67,7 +73,38 @@ void MainWindow::timestep()
     t.start();
     int target = 29;
 
-    world.timestep(0.001*target);
+    {
+        world.timestep(0.001*target);
+
+
+
+        std::vector<pPlayer> players;
+        BOOST_FOREACH(Players::value_type v, world.players)
+            players.push_back(v.second);
+
+        qSort(players.begin(), players.end(), highScore);
+
+        qint64 now = QDateTime::currentDateTime().toMSecsSinceEpoch();
+
+        QStringList str;
+        BOOST_FOREACH(pPlayer v, players)
+        {
+            Player& p = *v;
+
+            QTime t(0,0,0,now-p.timestamp);
+
+            str << QString("%1. %2 (id=%3, %4, %5) %6")
+                .arg(p.score)
+                .arg(p.name())
+                .arg(p.id())
+                .arg(QTime().addMSecs(now-p.timestamp).toString(Qt::ISODate))
+                .arg(QTime().addMSecs(p.playtime+=p.alive?target:0).toString(Qt::ISODate))
+                .arg(p.alive?"":" (observer)");
+        }
+
+        ui->listWidget->clear();
+        ui->listWidget->addItems(str);
+    }
 
     int left = target - t.elapsed();
     if (left < 1) left = 1;
