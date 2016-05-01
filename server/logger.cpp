8057dbaccff4cc7e26c8e30d24d81d5ca7d3c3ae
Johan <johan@patrick>
2016-05-01 17:23:16 +0200

    Keep the log file open.

--- server/logger.cpp
+++ server/logger.cpp
@@ -3,14 +3,20 @@
 #include <QFile>
 #include <QTextStream>
 #include <QDateTime>
+#include <memory>
 
 void Logger::logMessage(const QString& text)
 {
-    QFile log("achtung.log");
-
-    if (log.open(QFile::Append))
+    static std::unique_ptr<QFile> log;
+    if (!log)
     {
-        QTextStream(&log)
+        log.reset(new QFile("achtung.log"));
+        log->open(QFile::Append);
+    }
+
+    if (log->isOpen())
+    {
+        QTextStream(log.get())
                 << QDateTime::currentDateTime().toString(Qt::ISODate) << " "
                 << text << "\n";
     }
